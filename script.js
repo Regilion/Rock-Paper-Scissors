57df56f5e441a68ed48c71ca420301acdf745206
Egbu Reginald <egbureginald9@gmail.com>
2023-08-24 07:03:16 +0100

    remove the logging to the console used in debugging

---
+++ script.js
@@ -0,0 +1,60 @@
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+const options = [ROCK, PAPER, SCISSORS];
+
+// A function to return either Rock, paper or scissors randomly
+function getComputerChoice () {
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex]
+};
+
+
+// A function that takes the player and computer selection and returns a string of the winner of that round
+function playRound(playerSelection, computerSelection) {
+    // Use prompt to get the input of the user
+    const playerChoice = (prompt("Choose your weapon")).toLowerCase();
+
+    playerSelection = playerChoice;
+    computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        return `It's a tie`;
+    }
+
+    else if (playerSelection === "rock" && computerSelection === "scissors" || 
+             playerSelection === "scissors" && computerSelection === "paper" || 
+             playerSelection === "paper" && computerSelection === "rock") {
+        return `You won! ${playerSelection} beats ${computerSelection}`
+    }
+
+
+    // else if (playerSelection === "" || playerSelection !== "rock") {
+        
+    // }
+
+    else {
+        return `You Lost! ${computerSelection} beats ${playerSelection}`
+    }
+};
+
+// make the function case insensitive 
+// return the result of the function call not logging them to the console
+// Write a function that keeps score of a 5-round game and reports a loser or win at the end
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(playRound());
+    }
+    // console.log(playRound());
+    // console.log(playRound());
+    // console.log(playRound());
+    // console.log(playRound());
+    // console.log(playRound());
+    // playRound();
+    // playRound();
+    // playRound();
+    // playRound();
+}
+
+game()
